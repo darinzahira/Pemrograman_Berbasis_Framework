cd022cebb9b4fb0d3589d85a0434f33fd1e86ee7
darinzahira <zahiradarin@gmail.com>
2021-03-31 13:23:14 +0700

    upload modul

---
+++ Modul 6/week6/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import Table from './Containers/Table'
+import CreateTodo from './Containers/CreateTodo'
+import registerServiceWorker from './registerServiceWorker';
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
+  f)(createStore)(MainReducer)
+ReactDOM.render(<Provider store={store}>
+  <CreateTodo />
+  <Table />
+</Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
