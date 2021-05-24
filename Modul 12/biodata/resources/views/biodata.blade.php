3bd7cc703f2a0cff09656ce2d6323cea462ffef4
darinzahira <zahiradarin@gmail.com>
2021-05-24 08:30:52 +0700

    'upload'

---
+++ Modul 12/biodata/resources/views/biodata.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Biodata Diri</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <h1 class="text-center">Biodata Diri</h1>
+        <img src="Me.jpeg" class="rounded mx-auto d-block" height="250" width="200"><br>
+        ​<table class="table table-striped table-hover">
+            <tr>
+                <td>Nama Lengkap</td>
+                <td>Darin Zahira Aflah</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>1841720033</td>
+            </tr>
+            <tr>
+                <td>Kelas/Absen</td>
+                <td>TI-3E/09</td>
+            </tr>
+            <tr>
+                <td>Program Studi</td>
+                <td>Teknik Informatika</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>Teknologi Informasi</td>
+            </tr>
+            <tr>
+                <td>Perguruan Tinggi</td>
+                <td>Politeknik Negeri Malang</td>
+            </tr>
+
+        </table>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+
+
+
+  </body>
+</html>
